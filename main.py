e3bd3c12d1b7d6ae7cdde7f27b2644f67d0c74fc
ptky <danielpataky2@gmail.com>
2026-02-26 18:27:03 +0100

    update, meg mindig nem mukodik

--- main.py
+++ main.py
@@ -19,13 +19,20 @@ async def search_devices():
           print(f"[{i+1}.] device: {devices[i]}")
      data = get_info(size=size)
      return devices[data-1]
-     
+      
 async def connect():
      choosen_device =  await search_devices()
+     address = choosen_device.address
      print(f"Successfully choosen: {choosen_device.name}")
-
-
-     
+     print(f"\nTrying to connect to {address}")
+     device = await BleakScanner.find_device_by_address(address,timeout=10)
+     if(device == None):
+          print("failuire")
+     else:
+          print("client found, connecting")
+          async with BleakClient(device) as c:
+               print(f"Client connection: = {c.is_connected}")     
+          print("\ndisconnected")
 
 if __name__ == "__main__":
      asyncio.run(connect())
