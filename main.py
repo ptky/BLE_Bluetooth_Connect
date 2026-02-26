4aea04d08a388a9a8698a5826f997f5cecd3ca18
ptky <danielpataky2@gmail.com>
2026-02-26 19:20:29 +0100

    kesz,mukodik,soha,tobbet,nem,csinalok,ilyet

--- main.py
+++ main.py
@@ -5,6 +5,12 @@
 import asyncio, bleak
 from bleak import BleakScanner,BleakClient
 
+
+# ez mindenkinek a sajatja ahonnan hostolja es akar uzenetet kuldeni 
+service_uuid = "12345678-1234-5678-1234-56789abcdef0"
+char_uuid = "abcdef01-2345-6789-abcd-ef0123456789"
+hard_coded_message = "Szia telefon"
+
 def get_info(size:int):
      qstn = int(input("Enter the number of the device you want to connect to: "))
      while qstn > size or qstn < 1:
@@ -19,7 +25,10 @@ async def search_devices():
           print(f"[{i+1}.] device: {devices[i]}")
      data = get_info(size=size)
      return devices[data-1]
-      
+
+def notification_handler(sender, data):
+    print(f"Notification from {sender}: {data.decode()}")
+
 async def connect():
      choosen_device =  await search_devices()
      address = choosen_device.address
@@ -31,7 +40,15 @@ async def connect():
      else:
           print("client found, connecting")
           async with BleakClient(device) as c:
-               print(f"Client connection: = {c.is_connected}")     
+               print(f"Client connection: = {c.is_connected}")
+               await c.start_notify(char_uuid, notification_handler)
+               qstn1 = input("send a message? Y/N: ")
+               if qstn1 == "y" or qstn1 == "Y":
+                    await c.write_gatt_char(char_uuid, hard_coded_message.encode())
+                    print("message sent (we hope)")
+
+
+               await asyncio.sleep(30)     
           print("\ndisconnected")
 
 if __name__ == "__main__":
